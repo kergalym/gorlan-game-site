4ba61afd1555f273f8eb1978b08d78f65f85d635
Kerimbekov Galym <kegalym2@mail.ru>
2024-10-16 04:26:19 +0500

    Added counter

--- js/scripts.js
+++ js/scripts.js
@@ -5,17 +5,6 @@
 */
 window.addEventListener('DOMContentLoaded', event => {
 
-    const sidebarWrapper = document.getElementById('sidebar-wrapper');
-    let scrollToTopVisible = false;
-    // Closes the sidebar menu
-    const menuToggle = document.body.querySelector('.menu-toggle');
-    menuToggle.addEventListener('click', event => {
-        event.preventDefault();
-        sidebarWrapper.classList.toggle('active');
-        _toggleMenuIcon();
-        menuToggle.classList.toggle('active');
-    })
-
     // Closes responsive menu when a scroll trigger link is clicked
     var scrollTriggerList = [].slice.call(document.querySelectorAll('#sidebar-wrapper .js-scroll-trigger'));
     scrollTriggerList.map(scrollTrigger => {
@@ -39,21 +28,6 @@ window.addEventListener('DOMContentLoaded', event => {
         }
     }
 
-    // Scroll to top button appear
-    document.addEventListener('scroll', () => {
-        const scrollToTop = document.body.querySelector('.scroll-to-top');
-        if (document.documentElement.scrollTop > 100) {
-            if (!scrollToTopVisible) {
-                fadeIn(scrollToTop);
-                scrollToTopVisible = true;
-            }
-        } else {
-            if (scrollToTopVisible) {
-                fadeOut(scrollToTop);
-                scrollToTopVisible = false;
-            }
-        }
-    })
 })
 
 function fadeOut(el) {
@@ -78,3 +52,42 @@ function fadeIn(el, display) {
         }
     })();
 };
+
+// Disable context menu
+document.addEventListener('contextmenu', event => event.preventDefault());
+
+
+// Set the date we're counting down to
+//document.getElementById("btn-download-windows").style.display = "inline";
+//document.getElementById("btn-download-linux").style.display = "inline";
+
+var countDownDate = new Date("Dec 31, 2026 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="count"
+  document.getElementById("count").innerHTML = days + " days " + hours + " hours "
+  + minutes + " minutes ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("coming").innerHTML = "";
+    document.getElementById("count").innerHTML = "";
+    document.getElementById("btn-download-windows").style.display = "inline";
+    document.getElementById("btn-download-linux").style.display = "inline";
+  }
+}, 1000);
